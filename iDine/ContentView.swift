18630a70b13bc586e07431c0a3a2a14bf5b9f6dc
Kid Kid <kidkid.gum@gmail.com>
2023-01-15 23:04:56 -0600

    Setup

---
+++ iDine/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Kid Kid on 1/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(GroupedListStyle())
+        }
+     
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Kid Kid on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
