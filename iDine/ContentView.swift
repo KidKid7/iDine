72ac3e2905883205fbda8deba2632620c5a1fa42
Kid Kid <kidkid.gum@gmail.com>
2023-01-15 23:40:04 -0600

    Building a menu using List

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Menu")
-            .listStyle(GroupedListStyle())
+            .listStyle(.grouped)
         }
      
     }
