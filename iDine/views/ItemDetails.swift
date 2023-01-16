b849ba72c9a54271dd6ea299213ddd4696950d85
Kid Kid <kidkid.gum@gmail.com>
2023-01-16 04:50:31 -0600

    Displaying a detail screen with NavigationLink

---
+++ iDine/views/ItemDetails.swift
@@ -0,0 +1,52 @@
+//
+//  ItemDetails.swift
+//  iDine
+//
+//  Created by Kid Kid on 1/16/23.
+//
+
+import SwiftUI
+
+struct ItemDetails: View {
+    let item: MenuItem
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(EdgeInsets(top: -10,
+                                        leading: 10,
+                                        bottom: 0,
+                                        trailing: 10))
+                
+                Text(item.photoCredit)
+                    .padding(4)
+                    .font(.caption)
+                    .background(.black.opacity(0.7))
+                    .foregroundColor(.white)
+                    .offset(x: -15, y: -5)
+            }
+       
+            Text(item.description)
+                .padding(8)
+                .font(.system(size: 14))
+            
+            Spacer()
+        }
+        // Use toolbar to fix jumping animation
+        // of navigation from child to parent
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text(item.name)
+            }
+        }
+    }
+}
+
+struct ItemDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetails(item: MenuItem.example)
+    }
+}
