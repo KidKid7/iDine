6dbe62a5d1dbe7051b71b45c7b060e7d44f6bbbd
Kid Kid <kidkid.gum@gmail.com>
2023-01-17 18:03:05 -0600

    Two-way bindings in SwiftUI

--- iDine/views/CheckoutView.swift
+++ iDine/views/CheckoutView.swift
@@ -12,17 +12,39 @@ struct CheckoutView: View {
     
     let paymentMethods = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentMethod = "Cash"
+    @State private var addLoyaltyCard = false
+    @State private var cardID = ""
+    
+    let tips = [10, 15, 25, 30, 0]
+    @State private var tip = 15
     
     var body: some View {
-        VStack {
-            Section {
-                Picker("Select payment method:", selection: $paymentMethod) {
+        Form {
+            Section() {
+                Picker("Select payment method", selection: $paymentMethod) {
                     ForEach(paymentMethods, id: \.self) {
                         Text($0)
                     }
                 }
+                Toggle("Add loyalty card", isOn: $addLoyaltyCard.animation())
+                if addLoyaltyCard {
+                    TextField("Enter your card ID", text: $cardID)
+                }
             }
             
+            Section("Add a tip?") {
+                Picker("In percentage", selection: $tip) {
+                    ForEach(tips, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }.pickerStyle(.segmented)
+            }
+            
+            Section("Total: $100") {
+                Button("Confirm") {
+                    
+                }.frame(maxWidth: .infinity)
+            }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
